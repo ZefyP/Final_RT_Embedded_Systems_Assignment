11a51d134fbc9474ff69e6fbdbb8c075cea93722
ZefyP <zefypi@gmail.com>
2023-01-13 19:01:35 +0000

    add SPI and RTOS chip config

--- setup/RTE/RTE_Components.h
+++ setup/RTE/RTE_Components.h
@@ -16,6 +16,8 @@
  */
 #define CMSIS_device_header "stm32f4xx.h"
 
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
 #define RTE_DEVICE_FRAMEWORK_CLASSIC
 #define RTE_DEVICE_HAL_COMMON
 #define RTE_DEVICE_HAL_CORTEX
@@ -23,7 +25,14 @@
 #define RTE_DEVICE_HAL_GPIO
 #define RTE_DEVICE_HAL_PWR
 #define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_SPI
 #define RTE_DEVICE_HAL_TIM
 #define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+#define RTE_Drivers_SPI1                /* Driver SPI1 */
+        #define RTE_Drivers_SPI2                /* Driver SPI2 */
+        #define RTE_Drivers_SPI3                /* Driver SPI3 */
+        #define RTE_Drivers_SPI4                /* Driver SPI4 */
+        #define RTE_Drivers_SPI5                /* Driver SPI5 */
+        #define RTE_Drivers_SPI6                /* Driver SPI6 */
 
 #endif /* RTE_COMPONENTS_H */
