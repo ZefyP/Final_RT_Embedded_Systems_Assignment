7c1ae4742fdd8b0d123f7e5a18f2732239e1d3f4
ZefyP <zefypi@gmail.com>
2023-01-13 20:38:06 +0000

    tidy up

--- setup/my_defines.c
+++ setup/my_defines.c
@@ -1,37 +1,34 @@
 #include "stm32f4xx.h"
 
-
 // Definition for the function to initialise the LED and button
 
 void Initialise_LED_and_button(void){
 	// Initialise GPIO Port for LEDs
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // Enable Port D clock
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // Enable Port D clock 
 	GPIOD->MODER |= GPIO_MODER_MODER12_0; // Port D.12 output - green LED
 	GPIOD->MODER |= GPIO_MODER_MODER14_0; // Port D.14 output - red LED
+	
 	//Initialise GPIO for push-button
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // Enable Port A clock
 }
 
 // Definition for the function to blink the LED
-void Red_LED(uint8_t LED_state){
+void Blink_LED(uint8_t LED_state){
 	if(LED_state == 1){ // Checks to see if the request is to turn the LED on or off
-		GPIOD->BSRR = 1<<14; // Turn on the red LED
+			GPIOD->BSRR = 1<<12; // Turn on the green LED
 	}
 	else{
-		GPIOD->BSRR = 1<<(14+16); // Turn off the red LED
+			GPIOD->BSRR = 1<<(12+16); // Turn off the green LED
 	}
 }
 
-
 // Definition for the function to blink the LED
-void Blink_LED(uint8_t LED_state){
-	uint32_t ii;
+void Red_LED(uint8_t LED_state){
 	if(LED_state == 1){ // Checks to see if the request is to turn the LED on or off
-		GPIOD->BSRR = 1<<12; // Turn on the green LED
+			GPIOD->BSRR = 1<<14; // Turn on the red LED
 	}
 	else{
-		GPIOD->BSRR = 1<<(12+16); // Turn off the green LED
+			GPIOD->BSRR = 1<<(14+16); // Turn off the red LED
 	}
-	
-	for(ii=0;ii<26000000;ii++){} // FOR loop to implement a 1 second delay with a clock speed of 168MHz
+			
 }
--- setup/my_defines.h
+++ setup/my_defines.h
@@ -1,3 +1,4 @@
+#include "stm32f4xx.h"
 void Initialise_LED_and_button(void); // Declaration for the function to initialise the LED
-void Blink_LED(uint8_t LED_state); // Declaration for the function to blink the LED Function Definitions
-void Red_LED(uint8_t); // Declaration for the function to turn the red LED on/off.
+void Blink_LED(uint8_t); // Declaration for the function to blink the LED
+void Red_LED(uint8_t); // Declaration for the function to blink the LED
